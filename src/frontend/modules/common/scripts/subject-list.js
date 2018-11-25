139f88dd53aeaca67e3784991294597bbe09e057
Mikołaj Jarmuła <mikolaj.jarmula@currency-one.com>
2018-11-25 06:36:02 +0100

    Prepare preview course

--- src/frontend/modules/common/scripts/subject-list.js
+++ src/frontend/modules/common/scripts/subject-list.js
@@ -21,37 +21,69 @@ const subjectList = [
       'Paleontology',
       'Biodiversity',
     ],
-  }, {
+  },
+  {
     id: 1,
     name: "Mathematics",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc."
-  }, {
+    description: {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel arcu non tellus finibus elementum. Phasellus mollis dolor aliquet vestibulum imperdiet. Donec mollis lobortis tincidunt. Etiam vehicula sapien in mattis ullamcorper. Mauris maximus lectus vitae arcu porta elementum. Aliquam vel elit et quam rhoncus posuere quis sit amet orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum."
+    },
+  },
+  {
     id: 2,
     name: "Science",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc."
-  }, {
+    description: {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel arcu non tellus finibus elementum. Phasellus mollis dolor aliquet vestibulum imperdiet. Donec mollis lobortis tincidunt. Etiam vehicula sapien in mattis ullamcorper. Mauris maximus lectus vitae arcu porta elementum. Aliquam vel elit et quam rhoncus posuere quis sit amet orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum."
+    },
+  },
+  {
     id: 3,
     name: "Health",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc."
-  }, {
+    description: {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel arcu non tellus finibus elementum. Phasellus mollis dolor aliquet vestibulum imperdiet. Donec mollis lobortis tincidunt. Etiam vehicula sapien in mattis ullamcorper. Mauris maximus lectus vitae arcu porta elementum. Aliquam vel elit et quam rhoncus posuere quis sit amet orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum."
+    },
+  },
+  {
     id: 4,
     name: "English",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc."
-  }, {
+    description: {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel arcu non tellus finibus elementum. Phasellus mollis dolor aliquet vestibulum imperdiet. Donec mollis lobortis tincidunt. Etiam vehicula sapien in mattis ullamcorper. Mauris maximus lectus vitae arcu porta elementum. Aliquam vel elit et quam rhoncus posuere quis sit amet orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum."
+    },
+  },
+  {
     id: 5,
     name: "Geography",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc."
-  }, {
+    description: {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel arcu non tellus finibus elementum. Phasellus mollis dolor aliquet vestibulum imperdiet. Donec mollis lobortis tincidunt. Etiam vehicula sapien in mattis ullamcorper. Mauris maximus lectus vitae arcu porta elementum. Aliquam vel elit et quam rhoncus posuere quis sit amet orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum."
+    },
+  },
+  {
     id: 6,
     name: "History",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc."
-  }, {
+    description: {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel arcu non tellus finibus elementum. Phasellus mollis dolor aliquet vestibulum imperdiet. Donec mollis lobortis tincidunt. Etiam vehicula sapien in mattis ullamcorper. Mauris maximus lectus vitae arcu porta elementum. Aliquam vel elit et quam rhoncus posuere quis sit amet orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum."
+    },
+  },
+  {
     id: 7,
     name: "Computer Science",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc."
-  }, {
+    description: {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel arcu non tellus finibus elementum. Phasellus mollis dolor aliquet vestibulum imperdiet. Donec mollis lobortis tincidunt. Etiam vehicula sapien in mattis ullamcorper. Mauris maximus lectus vitae arcu porta elementum. Aliquam vel elit et quam rhoncus posuere quis sit amet orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum."
+    },
+  },
+  {
     id: 8,
     name: "Art",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc."
+    description: {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elit quis velit egestas dapibus. Aliquam dapibus feugiat ultrices. Nunc.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel arcu non tellus finibus elementum. Phasellus mollis dolor aliquet vestibulum imperdiet. Donec mollis lobortis tincidunt. Etiam vehicula sapien in mattis ullamcorper. Mauris maximus lectus vitae arcu porta elementum. Aliquam vel elit et quam rhoncus posuere quis sit amet orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum."
+    },
   }
 ];
